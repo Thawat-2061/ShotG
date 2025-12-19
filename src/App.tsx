0d6418c7c2965500e9d5c3ffd0fb4a09f6ce615e
Thawat-2061 <65011212061@msu.ac.th>
2025-12-19 15:25:16 +0700

    1of1

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
+
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+// import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <nav
+        // style={{
+        //   padding: 10,
+        //   background: "#eee",
+        //   display: "flex",
+        //   gap: "15px",
+        // }}
+      >
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link> */}
+      </nav>
+
+      <Routes>
+    
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/home" element={<Home />} /> */}
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/About.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div style={{ padding: 20 }}><h1>About Page</h1></div>;
+}
