b4020de5f56d5db38448a7f1de5a672fc9ffaa44
Thawat-2061 <65011212061@msu.ac.th>
2025-12-31 20:25:44 +0700

    dddd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,45 +11,45 @@ interface SlideItem {
 }
 
 /* ================= Images ================= */
-import img01 from "../assets/images/01.webp";
-import img03 from "../assets/images/03.webp";
-import img04 from "../assets/images/04.webp";
-import img07 from "../assets/images/07.webp";
-import img08 from "../assets/images/08.webp";
-import img09 from "../assets/images/09.jpg";
-import img12 from "../assets/images/12.jpg";
-import img13 from "../assets/images/13.jpg";
-import img14 from "../assets/images/14.webp";
-import img23 from "../assets/images/23.webp";
-import img24 from "../assets/images/24.jpg";
+import img01 from "../public/images/01.webp";
+import img03 from "../public/images/03.webp";
+import img04 from "../public/images/04.webp";
+import img07 from "../public/images/07.webp";
+import img08 from "../public/images/08.webp";
+import img09 from "../public/images/09.jpg";
+import img12 from "../public/images/12.jpg";
+import img13 from "../public/images/13.jpg";
+import img14 from "../public/images/14.webp";
+import img23 from "../public/images/23.webp";
+import img24 from "../public/images/24.jpg";
 
 /* ================= Videos ================= */
-import vid02 from "../assets/videos/02.mp4";
-import vid05 from "../assets/videos/05.mp4";
-import vid06 from "../assets/videos/06.mp4";
-import vid10 from "../assets/videos/10.mp4";
-import vid11 from "../assets/videos/11.mp4";
-import vid15 from "../assets/videos/15.mp4";
-import vid16 from "../assets/videos/16.mp4";
-import vid17 from "../assets/videos/17.mp4";
-import vid18 from "../assets/videos/18.mp4";
-import vid19 from "../assets/videos/19.mp4";
-import vid20 from "../assets/videos/20.mp4";
-import vid25 from "../assets/videos/25.mp4";
-import vid26 from "../assets/videos/26.mp4";
-import vid27 from "../assets/videos/27.mp4";
-import vid28 from "../assets/videos/28.mp4";
-import vid29 from "../assets/videos/29.mp4";
-import vid30 from "../assets/videos/30.mp4";
-import vid31 from "../assets/videos/31.mp4";
-import vid32 from "../assets/videos/32.mp4";
-import vid33 from "../assets/videos/33.mp4";
-import vid34 from "../assets/videos/34.mp4";
-import vid35 from "../assets/videos/35.mp4";
-import vid36 from "../assets/videos/36.mp4";
+import vid02 from "../public/videos/02.mp4";
+import vid05 from "../public/videos/05.mp4";
+import vid06 from "../public/videos/06.mp4";
+import vid10 from "../public/videos/10.mp4";
+import vid11 from "../public/videos/11.mp4";
+import vid15 from "../public/videos/15.mp4";
+import vid16 from "../public/videos/16.mp4";
+import vid17 from "../public/videos/17.mp4";
+import vid18 from "../public/videos/18.mp4";
+import vid19 from "../public/videos/19.mp4";
+import vid20 from "../public/videos/20.mp4";
+import vid25 from "../public/videos/25.mp4";
+import vid26 from "../public/videos/26.mp4";
+import vid27 from "../public/videos/27.mp4";
+import vid28 from "../public/videos/28.mp4";
+import vid29 from "../public/videos/29.mp4";
+import vid30 from "../public/videos/30.mp4";
+import vid31 from "../public/videos/31.mp4";
+import vid32 from "../public/videos/32.mp4";
+import vid33 from "../public/videos/33.mp4";
+import vid34 from "../public/videos/34.mp4";
+import vid35 from "../public/videos/35.mp4";
+import vid36 from "../public/videos/36.mp4";
 
 /* ================= Music ================= */
-import bgMusic from "../assets/music/wake.mp3";
+import bgMusic from "../public/music/wake.mp3";
 
 /* ================= Media Map ================= */
 const mediaMap: Record<string, { type: SlideType; src: string }> = {
